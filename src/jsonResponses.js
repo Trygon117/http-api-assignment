0c6f7bfb1410088d26804ef2afe43c1359981660
Abraham Furlan <abrahamfurlan@gmail.com>
2022-09-18 17:17:15 -0400

    fixed testing problems

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,113 +1,106 @@
 const url = require('url');
 
-//function to respond with a json object
-//takes request, response, status code and object to send
+// function to respond with a json object
+// takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const createResponseJSON = (message, id) => {
-    if (id === 'ok') {
-        return {
-            message,
-        };
-    } else {
-        return {
-            message,
-            id,
-        };
-    }
+  if (id === 'ok') {
+    return {
+      message,
+    };
+  }
+  return {
+    message,
+    id,
+  };
 };
 
 const success = (request, response) => {
-    let statusCode = 200;
-    let message = "This is a successful response";
-    let id = "ok";
+  const statusCode = 200;
+  const message = 'This is a successful response';
+  const id = 'ok';
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const badRequest = (request, response) => {
-    let statusCode = 200;
-    let message = "Missing valid query parameter set to true.";
-    let id = "ok";
+  let statusCode = 200;
+  let message = 'Missing valid query parameter set to true.';
+  let id = 'ok';
 
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    //console.log(parsedUrl.query);
+  // console.log(parsedUrl.query);
 
-    if (parsedUrl.query === 'valid=true') {
-        statusCode = 200;
-        message = 'This request has the required parameter.';
+  if (parsedUrl.query === 'valid=true') {
+    statusCode = 200;
+    message = 'This request has the required parameter.';
+  } else {
+    statusCode = 400;
+    id = 'BadRequest';
+  }
 
-    } else {
-        statusCode = 400;
-        id = 'BadRequest';
-    }
-
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
-
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const unauthorized = (request, response) => {
-    let statusCode = 200;
-    let message = "Missing loggedIn query parameter set to yes.";
-    let id = "ok";
+  let statusCode = 200;
+  let message = 'Missing loggedIn query parameter set to yes.';
+  let id = 'ok';
 
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    //console.log(parsedUrl.query);
+  // console.log(parsedUrl.query);
 
-    if (parsedUrl.query === 'loggedIn=yes') {
-        statusCode = 200;
-        message = 'You have successfully viewed the content.';
-    } else {
-        statusCode = 401;
-        id = 'Unauthorized';
-    }
+  if (parsedUrl.query === 'loggedIn=yes') {
+    statusCode = 200;
+    message = 'You have successfully viewed the content.';
+  } else {
+    statusCode = 401;
+    id = 'Unauthorized';
+  }
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
-
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const forbidden = (request, response) => {
-    let statusCode = 403;
-    let message = "You do not have access to this content.";
-    let id = "Forbidden";
+  const statusCode = 403;
+  const message = 'You do not have access to this content.';
+  const id = 'Forbidden';
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
-
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const internal = (request, response) => {
-    let statusCode = 500;
-    let message = "Internal Server Error. Something went wrong.";
-    let id = "InternalServerError";
+  const statusCode = 500;
+  const message = 'Internal Server Error. Something went wrong.';
+  const id = 'InternalServerError';
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
-
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const notImplemented = (request, response) => {
-    let statusCode = 501;
-    let message = "A get request for this page has not been implemented yet. Check again later for updated Content.";
-    let id = "NotImplemented";
+  const statusCode = 501;
+  const message = 'A get request for this page has not been implemented yet. Check again later for updated Content.';
+  const id = 'NotImplemented';
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
-
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 const notFound = (request, response) => {
-    let statusCode = 404;
-    let message = "The page you are looking for was not found.";
-    let id = "NotFound";
+  const statusCode = 404;
+  const message = 'The page you are looking for was not found.';
+  const id = 'NotFound';
 
-    respondJSON(request, response, statusCode, createResponseJSON(message, id));
+  respondJSON(request, response, statusCode, createResponseJSON(message, id));
 };
 
-//public exports
+// public exports
 module.exports = {
-    success,
-    badRequest,
-    unauthorized,
-    forbidden,
-    internal,
-    notImplemented,
-    notFound,
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
 };
--- src/xmlResponses.js
+++ src/xmlResponses.js
@@ -1,107 +1,100 @@
 const url = require('url');
 
-//function to respond with a json object
-//takes request, response, status code and object to send
+// function to respond with a json object
+// takes request, response, status code and object to send
 const respondXML = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'text/xml' });
-    response.write(object);
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'text/xml' });
+  response.write(object);
+  response.end();
 };
 
 const createResponseXML = (message, id) => {
-    if (id === 'ok') {
-        return `<response><message>${message}</message></response>`;
-    } else {
-        return `<response><message>${message}</message><id>${id}</id></response>`;
-    }
+  if (id === 'ok') {
+    return `<response><message>${message}</message></response>`;
+  }
+  return `<response><message>${message}</message><id>${id}</id></response>`;
 };
 
 const success = (request, response) => {
-    let statusCode = 200;
-    let message = "This is a successful response";
-    let id = "ok";
+  const statusCode = 200;
+  const message = 'This is a successful response';
+  const id = 'ok';
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const badRequest = (request, response) => {
-    let statusCode = 200;
-    let message = "Missing valid query parameter set to true.";
-    let id = "ok";
+  let statusCode = 200;
+  let message = 'Missing valid query parameter set to true.';
+  let id = 'ok';
 
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    //console.log(parsedUrl.query);
+  // console.log(parsedUrl.query);
 
-    if (parsedUrl.query === 'valid=true') {
-        statusCode = 200;
-        message = 'This request has the required parameter.';
-    } else {
-        statusCode = 400;
-        id = 'BadRequest';
-    }
+  if (parsedUrl.query === 'valid=true') {
+    statusCode = 200;
+    message = 'This request has the required parameter.';
+  } else {
+    statusCode = 400;
+    id = 'BadRequest';
+  }
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
-
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const unauthorized = (request, response) => {
-    let statusCode = 200;
-    let message = "Missing loggedIn query parameter set to yes.";
-    let id = "ok";
+  let statusCode = 200;
+  let message = 'Missing loggedIn query parameter set to yes.';
+  let id = 'ok';
 
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    //console.log(parsedUrl.query);
+  // console.log(parsedUrl.query);
 
-    if (parsedUrl.query === 'loggedIn=yes') {
-        statusCode = 200;
-        message = 'You have successfully viewed the content.';
+  if (parsedUrl.query === 'loggedIn=yes') {
+    statusCode = 200;
+    message = 'You have successfully viewed the content.';
+  } else {
+    statusCode = 401;
+    id = 'Unauthorized';
+  }
 
-    } else {
-        statusCode = 401;
-        id = 'Unauthorized';
-    }
-
-    respondXML(request, response, statusCode, createResponseXML(message, id));
-
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const forbidden = (request, response) => {
-    let statusCode = 403;
-    let message = "You do not have access to this content.";
-    let id = "Forbidden";
+  const statusCode = 403;
+  const message = 'You do not have access to this content.';
+  const id = 'Forbidden';
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
-
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const internal = (request, response) => {
-    let statusCode = 500;
-    let message = "Internal Server Error. Something went wrong.";
-    let id = "InternalServerError";
+  const statusCode = 500;
+  const message = 'Internal Server Error. Something went wrong.';
+  const id = 'InternalServerError';
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
-
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const notImplemented = (request, response) => {
-    let statusCode = 501;
-    let message = "A get request for this page has not been implemented yet. Check again later for updated Content.";
-    let id = "NotImplemented";
+  const statusCode = 501;
+  const message = 'A get request for this page has not been implemented yet. Check again later for updated Content.';
+  const id = 'NotImplemented';
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
-
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
 const notFound = (request, response) => {
-    let statusCode = 404;
-    let message = "The page you are looking for was not found.";
-    let id = "NotFound";
+  const statusCode = 404;
+  const message = 'The page you are looking for was not found.';
+  const id = 'NotFound';
 
-    respondXML(request, response, statusCode, createResponseXML(message, id));
+  respondXML(request, response, statusCode, createResponseXML(message, id));
 };
-//public exports
+// public exports
 module.exports = {
-    success,
-    badRequest,
-    unauthorized,
-    forbidden,
-    internal,
-    notImplemented,
-    notFound,
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
 };
